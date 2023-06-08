e41b78d734be3f2858cbfbfd78ac908c52f00ada
LaChancee <miharisoababef@gmail.com>
2023-06-08 09:16:53 +0200

    Ajout des context dans la page d'accueil

---
+++ src/screen/HomeScreen.jsx
@@ -0,0 +1,19 @@
+import {Text} from "react-native"
+import {View} from "react-native"
+import React, {useContext} from "react";
+
+const HomeScreen = () => {
+
+
+    const {userData, setUserData} = useContext(UserContext);
+
+
+    return (
+        <View>
+            <Text>
+                Salut à toi {userData.username}
+            </Text>
+        </View>
+    )
+}
+export default HomeScreen
