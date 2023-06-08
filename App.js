dbb7846b8cd030c1377904a93bb66b53aa4817f5
LaChancee <miharisoababef@gmail.com>
2023-06-08 09:17:03 +0200

    Ajout des context

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import Home from "./src/screen/Home";
 import LoginScreen from "./src/screen/LoginScreen";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
+import Register from "./src/screen/Register";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from '@react-navigation/stack';
+import UserContextProviders, {UserContext} from "./src/contexts/UserContext";
+import Layout from "./src/Layout/Layout";
+import userContext from "./src/contexts/UserContext";
+import {useEffect} from "react";
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    const {data, updateData} = userContext(UserContext)
+    useEffect(() => {
+        console.log(data);
+        updateData({
+            authenticate: true
+        })
+    }, [data])
+
+    return (
+        <UserContextProviders>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Login" component={LoginScreen}/>
+                    <Stack.Screen name="Register" component={Register}/>
+                    <Stack.Screen
+                        name="Layout"
+                        component={Layout}
+                        options={{
+                            headerShown: false, // Masquer l'en-tête (header)
+                            initialParams: {user: data},
+                        }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </UserContextProviders>
+    );
 }
